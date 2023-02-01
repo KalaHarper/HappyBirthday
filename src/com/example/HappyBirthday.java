18131b35d9ed5b3686922df231e98296ff5d46a4
Kala Harper <kala.n.harper@gmail.com>
2023-02-01 13:32:59 -0600

    Very base code with English and Spanish

--- src/com/example/HappyBirthday.java
+++ src/com/example/HappyBirthday.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class HappyBirthday {
     public static void main(String[] args) {
-        System.out.println("Happy Birthday");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Please choose a language: \n a) English \n b) Spanish");
+
+        String selection = input.nextLine().toLowerCase(Locale.ROOT);
+
+        if(selection.contains("a")){
+            System.out.println("Happy birthday to you!\n Happy birthday to you!\n Happy birthday dear " + name + "!\n Happy birthday to you!");
+        }else if(selection.contains("b")){
+            System.out.println("Feliz cumpleaños a ti\n" +
+                    "feliz cumpleaños a ti\n" +
+                    "feliz cumpleaños " + name + "!\n" +
+                    "feliz cumpleaños a ti.");
+        }else{
+            System.out.println("Selection invalid please try again");
+        }
+        //System.out.println("You chose " + selection);
     }
 }
